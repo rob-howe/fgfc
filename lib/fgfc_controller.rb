a11853e936d7f7b6205283d22aaf90dbd34e3f10
rob <robert.howe@gmail.com>
2013-11-23 22:13:53 +0000

    Removed old site and added down for maintenance

--- lib/fgfc_controller.rb
+++ lib/fgfc_controller.rb
@@ -3,23 +3,23 @@ require 'sinatra/base'
 
 class FgfcController < Sinatra::Base
 
-  get '/' do
-     erb:home
+  get '/*' do
+     erb :down, :layout => false
   end
 
-  get '/pages/managersConduct' do
-    erb :managersConduct, :layout => false
-  end
-
-  get '/pages/playersConduct' do
-    erb :managersConduct, :layout => false
-  end
-
-
-  get '/pages/:page' do
-    @redbacks = params[:page].start_with? 'redbacks'
-    erb :"#{params[:page]}"
-  end
+  #get '/pages/managersConduct' do
+  #  erb :managersConduct, :layout => false
+  #end
+  #
+  #get '/pages/playersConduct' do
+  #  erb :managersConduct, :layout => false
+  #end
+  #
+  #
+  #get '/pages/:page' do
+  #  @redbacks = params[:page].start_with? 'redbacks'
+  #  erb :"#{params[:page]}"
+  #end
 
     # same as:   error 404
   not_found do
