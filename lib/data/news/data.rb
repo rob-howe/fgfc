68a095382a97fd26fc03e9a83ece55e29c41d40e
rob <robert.howe@gmail.com>
2014-12-20 19:13:59 +0000

    news update

--- lib/data/news/data.rb
+++ lib/data/news/data.rb
@@ -4,17 +4,9 @@ module FGFC
     @news = []
 
     @news << {
-        type: 'team',
-        heading: "U11 Black - Goalkeeper Wanted",
-        para1: 'Forest Glade U11 Black require a Goalkeeper to join our current squad. We play in the Southend and District Youth League E Division. Our Coaches are all CRB checked and are dedicated to helping the boys develop as individuals whilst enjoying the game as a team.',
-        para2: 'We train every Saturday at our Barleylands ground. We are looking for a player with previous team experience and in school year 6.',
-        para3: 'If interested please contact Nick Bailey on 07751 344644 or email nandkbailey@sky.com'
-    }
-
-    @news << {
         type: 'important',
-        heading: "Fives And Presentation Day",
-        para1: '<iframe height="440" width="340" frameborder="0" src="//flipagram.com/f/EHgRDLjiDP/embed"></iframe>',
+        heading: "Happy Christmas & New Year",
+        para1: 'Wishing everyone a happy Christmas and New Year from all at Forest Glade.',
         para2: '',
         para3: ''
     }
@@ -32,14 +24,6 @@ module FGFC
         heading: 'Next Committee Meeting',
         para1: 'The committee meet every month.',
         para2: "If there are any issues you'd like raised please contact the club secretary at sereatry@forestgladefc.co.uk or contact your team manager.",
-        para3: ''
-    }
-
-    @news << {
-        type: 'team',
-        heading: 'Girl players wanted',
-        para1: 'Forest Glade girls are looking for new players in school years 8 & 9.',
-        para2: "If interested please contact Laura on 07817 542812",
         para3: ''
     }
 
