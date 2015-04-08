b51edc08fe107dfb715a65318f8da36513f7067b
rob <robert.howe@gmail.com>
2015-04-08 21:06:34 +0100

    U11 black advert

--- lib/data/news/data.rb
+++ lib/data/news/data.rb
@@ -5,6 +5,13 @@ module FGFC
 
     @news << {
         type: 'team',
+        heading: "U11 Black - Players Wanted",
+        para1: "Forest Glade U11 Black are looking to strengthen our squad for next season at U12 (School Year 7 Sept 2015) level.",
+        para2: "We are looking for players with experience of playing within a team.",
+        para3: "Please contact Nick Bailey on 07751 344644 for further details."
+    }
+    @news << {
+        type: 'team',
         heading: "Player Trials U10's",
         para1: "Forest Glade U10's will be holding trials to strengthen their already successful squad on the 25th April 2015. This is due to the format changing from 7 to 9 aside at this age group next season.",
         para2: "Our Under 10's currently play in the top division of the Southend District League so experience at this level would be an advantage.",
