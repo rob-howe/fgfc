59474d906fcbb8ee379d88040b5b797c907a9832
bskyb-mysky <mysky@bskyb.com>
2015-04-26 22:18:55 +0100

    U11 black advert

--- lib/data/news/data.rb
+++ lib/data/news/data.rb
@@ -5,24 +5,26 @@ module FGFC
 
     @news << {
         type: 'team',
-        heading: "U11 Black - Players Wanted",
-        para1: "Forest Glade U11 Black are looking to strengthen our squad for next season at U12 (School Year 7 Sept 2015) level.",
-        para2: "We are looking for players with experience of playing within a team.",
-        para3: "Please contact Nick Bailey on 07751 344644 for further details."
+        heading: "U11 Black - Striker Wanted",
+        para1: "Forest Glade U12 Black who play in the Southend and District Youth League are looking for a striker for next season.",
+        para2: "We are a FA Charter Standard Club. Our manager and coach are CRB checked and FA Level 1 qualified.",
+        para3: "If you are school year 7 in September and have league goalscoring ability then please call Nick Bailey on 07751 344644 or email nandkbailey@sky.com"
     }
-    @news << {
-        type: 'team',
-        heading: "Player Trials U10's",
-        para1: "Forest Glade U10's will be holding trials to strengthen their already successful squad on the 25th April 2015. This is due to the format changing from 7 to 9 aside at this age group next season.",
-        para2: "Our Under 10's currently play in the top division of the Southend District League so experience at this level would be an advantage.",
-        para3: "Please contact Steve Bott (Manager) (FA level 2 qualified) for further details either by email Stephen.bott@tsb.co.uk or phone 07966 485 644"
-    }
+
     @news << {
         type: 'important',
         heading: "2015 Forest Glade 5's",
         para1: 'The 2015 Forest Glade 5\'s will take place on Saturday 23rd and Sunday 24th May 2014.',
-        para2: 'Registration is now open.',
+        para2: 'Registration is now closed.',
         para3: 'Please visit <a href="http://www.thefg5s.co.uk">www.theFG5s.co.uk</a> for more information.'
+    }
+
+@news << {
+        type: '',
+        heading: '',
+        para1: '',
+        para2: '',
+        para3: ''
     }
 
     @news << {
