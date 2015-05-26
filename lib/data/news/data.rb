7a2fab336993cceae2c533872c04a1d900bd7049
bskyb-mysky <mysky@bskyb.com>
2015-05-26 17:45:51 +0100

    U16  advert & removal of fg5s

--- lib/data/news/data.rb
+++ lib/data/news/data.rb
@@ -5,26 +5,18 @@ module FGFC
 
     @news << {
         type: 'team',
+        heading: "U16 players wanted",
+        para1: "Forest Glade FC U16 for season 2015 /2016 are looking for experienced players to strengthen their two existing squads.",
+        para2: "The teams play in the 2 top divisions of the Southend and District Junior Sunday league. Training is on Saturday mornings with Games on a Sunday.",
+        para3: "For details please contact either Darren (07890 576950), secretary@forestgladefc.co.uk for Forest Glade Blue or Paul on (07786 824678), paulstephwarren@msn.com for Forest Glade White."
+    }
+
+    @news << {
+        type: 'team',
         heading: "U12 Black - Striker Wanted",
         para1: "Forest Glade U12 Black who play in the Southend and District Youth League are looking for a striker for next season.",
         para2: "We are a FA Charter Standard Club. Our manager and coach are CRB checked and FA Level 1 qualified.",
         para3: "If you are school year 7 in September and have league goalscoring ability then please call Nick Bailey on 07751 344644 or email nandkbailey@sky.com"
-    }
-
-    @news << {
-        type: 'important',
-        heading: "2015 Forest Glade 5's",
-        para1: 'The 2015 Forest Glade 5\'s will take place on Saturday 23rd and Sunday 24th May 2014.',
-        para2: 'Registration is now closed.',
-        para3: 'Please visit <a href="http://www.thefg5s.co.uk">www.theFG5s.co.uk</a> for more information.'
-    }
-
-@news << {
-        type: '',
-        heading: '',
-        para1: '',
-        para2: '',
-        para3: ''
     }
 
     @news << {
